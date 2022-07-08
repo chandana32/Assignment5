ca4fbddf9140b8bbfb4caffa69c3066593cc20e4
Chandana32 <itzmechand32@gmail.com>
2022-07-08 22:58:47 +0530

    second commit

---
+++ Assignment5/Data/EmployeeDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Assignment5.Models;
+
+namespace Assignment5.Data
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext (DbContextOptions<EmployeeDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assignment5.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ Assignment5/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment5.Models
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        [Key]
+        public int EId { get; set; }
+        public string EName { get; set; }
+
+        public string EDesign { get; set; }
+
+        public DateTime? EDOJ { get; set; }
+
+    }
+}
+
